1c1e35c440a7bd6cff4a688597546cb40417b118
Niqoo10 <nicopanella2009@hotmail.com>
2023-08-30 12:37:28 -0300

    Branch develop

--- Test/test_board.py
+++ Test/test_board.py
@@ -14,6 +14,16 @@ class TestBoard(unittest.TestCase):
             15,
         )
 
+class TesteBoardAndValues(unittest.TestCase):
+     def test_init_different_multipliers_and_letters(self):
+        board = Board()
+        assert len(board.grid) == 15
+        assert len(board.grid[0]) == 15
+        for row in board.grid:
+            for cell in row:
+                assert cell.multiplier != 0
+                assert cell.multiplier_type in ['', 'letter']
+                assert cell.letter is None
 
 if __name__ == '__main__':
     unittest.main()
