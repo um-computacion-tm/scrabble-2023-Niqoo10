1f03e13edc6718a4c466fe92b43936f7b27e2212
Niqoo10 <nicopanella2009@hotmail.com>
2023-09-05 17:05:26 -0300

    Add calculate word

--- Game/board.py
+++ Game/board.py
@@ -6,3 +6,14 @@ class Board:
             [ Cell(1, '') for _ in range(15) ]
             for _ in range(15)
         ]
+
+    def calculate_word_value(self, word):
+        points = 0
+        word_multiplier = 1
+        for cell in word:
+            if not(cell.letter_multiplier) and cell.active:
+                word_multiplier *= cell.multiplier
+            points += cell.calculate_value()
+            cell.active = False
+        points = points * word_multiplier
+        return points
